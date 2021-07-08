dcd119f1087d753adc4c5d9ea965166e034dee26
askandwa <askandwa@LIN51013677.corp.capgemini.com>
2021-07-08 10:28:21 +0530

    commit

--- gitproject/src/main/java/gitproject/Login.java
+++ gitproject/src/main/java/gitproject/Login.java
@@ -5,6 +5,7 @@ public class Login {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Just Logged In");
+		System.out.println("Just checking a change");
 
 	}
 
